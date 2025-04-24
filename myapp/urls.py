cb6695afa826cd0b6fe2323686222f68978fc2a7
pjjj23 <pjjbonbon@gmail.com>
2025-04-24 21:38:48 +0800

    first

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),  # Home page
+    path('', views.home, name='home'),  # Home page 
+    path('gallery/', views.gallery, name='gallery'),
+    path('lily/', views.lily, name='lily'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'index.html')
+
+def gallery(request):
+    return render(request, 'gallery.html')
+
+def lily(request):
+    return render(request, 'lily.html')
